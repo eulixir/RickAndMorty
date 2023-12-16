f07cbd2d33899c12220ae59bb0c751e2a7e9a668
João Pedro Alves <joaopealves.dev@gmail.com>
2023-12-16 13:48:21 -0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by João Pedro Alves on 16/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

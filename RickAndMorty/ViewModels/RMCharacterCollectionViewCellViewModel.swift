5397a1da10f5265f761de601762f9d2807027f83
João Pedro Alves <joaopealves.dev@gmail.com>
2023-12-18 22:13:52 -0300

    chore: create card view

---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by João Pedro Alves on 18/12/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMCharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by João Pedro Alves on 18/12/23.
+//
+
+import UIKit
+
+class RMCharacterCollectionViewCell: UIViewCollection {
+
+}
